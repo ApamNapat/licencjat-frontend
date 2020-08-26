a66be2d8a15b04307c8b0c06feb5eddc5c12beea
BB <bartlomiejbetka@gmail.com>
2020-08-26 21:05:05 +0200

    Extended about

--- src/Components/About.js
+++ src/Components/About.js
@@ -9,16 +9,18 @@ const About = () => {
             <p>Every semester lasts 14 "days". Through <b>Set Timetable</b> you can set your actions for up to 12
                 upcoming hours and then monitor your progress at <b>Your Timetable</b>.</p>
             <p>After 14 days the semester ends, you will be able to take an exam for every class which you have attended
-                at least 10 times. Keep in mind that the classes from higher semesters are harder. If you get enough
-                ECTS points you will move into a next semester, otherwise you will stay at the current semester, but
-                once you fail a second time, you're out!</p>
+                at least 10 times. Your scores depend on the abilities you earned and the skills that the class
+                improves. Keep in mind that the classes from higher semesters are harder, and the ones from
+                lower semesters won't teach you as much. If you get enough ECTS points you will move into a next
+                semester, otherwise you will stay at the current semester, but once you fail a second time, you're
+                out!</p>
             <p>Aside from learning and attending classes you also have to take care of yourself! Your mood influences
                 your performance at most actions. And while you can keep on going even at low energy
                 once it drops below 10 you won't be able to go on.
             </p>
             <p>Eating, drinking and a roof over your head are not free, so remember to work, at the end of each day you
                 will be charged for all that and if you can't afford it - the stress will lower your Energy and Mood</p>
-            <p>Attending classes, also gives you a chance to learn new abilities, which you can see at <b>Abilities</b>,
+            <p>Attending classes also gives you a chance to learn new abilities, which you can see at <b>Abilities</b>,
                 those are nice to collect and, more importantly, are very important once it comes to passing the exams.
             </p>
             <p>And if you're every wondering what exactly has been happening after each of your actions - check out
